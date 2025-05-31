af8ab411f0274c6eae43a22a7bb76170a2f53820
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-06-01 01:47:32 +0300

    latest updates on models and serializers

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,13 +1,2 @@
 from django.shortcuts import render
 
-# Create your views here.
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .models import Listing
-from .serializers import ListingSerializer
-
-@api_view(["GET"])
-def get_listings(request):
-    listings = Listing.objects.all()
-    serializer = ListingSerializer(listings, many=True)
-    return Response(serializer.data)
